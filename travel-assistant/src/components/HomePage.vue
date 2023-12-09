7455012a5be089ec7478c9928b311258a3b31700
Dobner Szabolcs-Imre <91784240+Dobnerke1669@users.noreply.github.com>
2023-12-09 21:12:22 +0200

    Accommodations and venues page (#2)
    * accommodation page with venues page made
    
    * added fields to accomodation page with very small styling

--- travel-assistant/src/components/HomePage.vue
+++ travel-assistant/src/components/HomePage.vue
@@ -5,8 +5,8 @@
     <div>
       <button @click="goTo('/account')">My Account</button>
       <button @click="goTo('/flights')">Flights</button>
-      <button>Accommodation</button>
-      <button>Venues</button>
+      <button @click="goTo('/accommodations')">Accommodation</button>
+      <button @click="goTo('/venues')">Venues</button>
     </div>
   </div>
 </template>
--- travel-assistant/src/router/index.js
+++ travel-assistant/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import FlightsPage from '../components/FlightsPage.vue'
+import AccommodationsPage from "@/components/AccommodationsPage.vue";
+import VenuesPage from "@/components/VenuesPage.vue";
 import AccountPage from '../components/AccountPage.vue'
 
 const routes = [
@@ -13,6 +15,16 @@ const routes = [
         path: '/flights',
         name: 'flights',
         component: FlightsPage
+    },
+    {
+        path: '/accommodations',
+        name: 'accommodations',
+        component: AccommodationsPage
+    },
+    {
+        path: '/venues',
+        name: 'venues',
+        component: VenuesPage
     },
     {
         path: '/account',
