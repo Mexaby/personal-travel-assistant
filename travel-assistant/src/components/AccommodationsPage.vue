bbd2512b28ae308d950bca0e2ba5de498c487f28
Dobnerke1669 <dobner_szaby@yahoo.com>
2024-01-05 15:13:34 +0200

    search accommodation page created with minimal "search" engine

--- travel-assistant/src/components/AccommodationsPage.vue
+++ travel-assistant/src/components/AccommodationsPage.vue
@@ -1,11 +1,29 @@
 <script>
-  export default {
-    methods: {
-      goTo(route) {
-        this.$router.push(route);
-      },
+export default {
+  methods: {
+    goTo(route) {
+      this.$router.push(route);
     },
-  };
+  },
+};
+</script>
+<script setup>
+import {ref} from "vue";
+
+let input = ref("");
+let showDestinations = ref(true);
+const destinations = ["Cluj-Napoca", "Alicante", "Barcelona", "Bucharest", "Budapest", "Copenhagen", "Dublin", "Iasi", "London", "Madrid", "Milan", "Paris", "Rome", "Stockholm", "Vienna", "Warsaw", "Zurich"];
+
+function filteredList() {
+  return destinations.filter((destination) =>
+      destination.toLowerCase().includes(input.value.toLowerCase())
+  );
+}
+
+function handleResultClick(destination) {
+  input.value = destination;
+  showDestinations.value = false;
+}
 </script>
 
 <template>
@@ -13,72 +31,155 @@
     <div>Accommodations Page</div>
     <button @click="goTo('/')">Back to Home</button>
     <br><br>
-    <input type="text" class="location" placeholder="Select your destination">
-    <input type="date" class="check-in" placeholder="Check-in">
-    <input type="date" class="check-out" placeholder="Check-out">
-    <input type="number" class="guests" placeholder="Guests">
-    <button class="search-button">Search</button>
+    <div class="inputs">
+      <div class="input-group">
+        <input type="text" class="location" v-model="input" placeholder="Search destinations..."/>
+        <input type="date" class="check-in" placeholder="Check-in">
+        <input type="date" class="check-out" placeholder="Check-out">
+        <input type="number" class="guests" placeholder="Guests">
+      </div>
+      <button @click="search" class="search-button">Search</button>
+    </div>
+    <div v-if="showDestinations && input !== ''" class="destinations-container">
+      <div
+          class="item destinations"
+          v-for="destination in filteredList()"
+          :key="destination"
+          @click="handleResultClick(destination)"
+      >
+        <p>{{ destination }}</p>
+      </div>
+      <div class="item error" v-if="input && !filteredList().length">
+        <p>No results found!</p>
+      </div>
+    </div>
   </div>
 
 </template>
 
 <style>
-  .location
-  {
-    width: 200px;
-    height: 30px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    padding: 5px;
-    margin: 5px;
-  }
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
 
-  .check-in
-  {
-    width: 100px;
-    height: 30px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    padding: 5px;
-    margin: 5px;
-  }
+.destinations-container {
+  width: 250px;
+  margin-left: 17.5%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .check-out
-  {
-    width: 100px;
-    height: 30px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    padding: 5px;
-    margin: 5px;
-  }
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
 
-  .guests
-  {
-    width: 100px;
-    height: 30px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    padding: 5px;
-    margin: 5px;
-  }
-  .search-button
-  {
-    width: 100px;
-    height: 30px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    padding: 5px;
-    margin: 5px;
-  }
+body {
+  padding: 20px;
+  min-height: 100vh;
+  background-color: rgb(234, 242, 255);
+}
 
-  .location:focus, .check-in:focus, .check-out:focus, .guests:focus, .search-button:focus
-  {
-    outline: none;
-  }
+.inputs {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
 
-  .check-out
-  {
-    margin-left: 5px;
-  }
+.input-group {
+  display: flex;
+  align-items: center;
+}
+
+.input-group input {
+  margin: 5px;
+}
+
+input {
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+  rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+}
+
+.item {
+  width: 250px;
+  margin: 0 auto 10px auto;
+  padding: 10px 20px;
+  color: white;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+  rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+}
+
+.destinations {
+  background-color: rgb(97, 62, 252);
+  cursor: pointer;
+  margin: 0;
+}
+
+.error {
+  width: 250px; /* Set the same width as the .item class */
+  background-color: tomato;
+  margin: 0 auto 10px auto; /* Maintain the same margin as .item */
+  padding: 10px 20px;
+  color: white;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+}
+
+.location {
+  background: white url("assets/search-icon.svg") no-repeat 0 center;
+  background-size: 15px 15px;
+  width: 250px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 5px 30px 5px 20px;
+  margin: 5px;
+}
+
+.check-in {
+  width: 200px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 5px;
+  margin: 5px;
+}
+
+.check-out {
+  width: 200px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 5px;
+  margin: 5px;
+}
+
+.guests {
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 5px;
+  margin: 5px;
+}
+
+.search-button {
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 5px;
+  margin: 5px;
+}
+
+.location:focus, .check-in:focus, .check-out:focus, .guests:focus, .search-button:focus {
+  outline: none;
+}
+
+.check-out {
+  margin-left: 5px;
+}
 </style>
