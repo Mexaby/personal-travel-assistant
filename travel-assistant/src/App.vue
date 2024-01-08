9e141db09f74ed4a310c947d72806b8e4680e377
Mexaby <csababa2001@gmail.com>
2024-01-08 23:49:01 +0200

    added page transition animation

--- travel-assistant/src/App.vue
+++ travel-assistant/src/App.vue
@@ -1,3 +1,32 @@
 <template>
-  <router-view />
+  <div class="active-page">
+    <router-view v-slot="{ Component }">
+      <transition name="zoom-fade" mode="out-in">
+        <component :is="Component" :key="$route.fullPath" />
+      </transition>
+    </router-view>
+  </div>
 </template>
+
+<style>
+.zoom-fade-enter-active,
+.zoom-fade-leave-active {
+  transition: all 0.5s;
+}
+
+.zoom-fade-enter-from {
+  transform: scale(0.5);
+  opacity: 0;
+}
+
+.zoom-fade-leave-to {
+  transform: scale(1.5);
+  opacity: 0;
+}
+
+.zoom-fade-enter-to,
+.zoom-fade-leave-from {
+  transform: scale(1);
+  opacity: 1;
+}
+</style>
